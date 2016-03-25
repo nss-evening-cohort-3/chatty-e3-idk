68a2f41190a636b92e93f746869a6f66b9e31ec0
Bradley Guthrie <guppy96@gmail.com>
2016-03-25 17:45:25 -0500

    delete conditions

--- delete.js
+++ delete.js
@@ -10,23 +10,53 @@ var Chatty = (function(originalChatty) {
 
 console.log("originalChatty", originalChatty);
 
-  originalChatty.deleteMessage = function(event){
-    this.removeChild(event.target.parentNode);
+  originalChatty.delete = function(event){
+    
+
+  if(event.target.id ==="button") {
+      this.removeChild(event.target.parentNode);
+    }
   }
 
+  console.log("delete");
   return originalChatty;
 
 })( Chatty || {} );
 
 
-  // function watchForDelete() {
-  //   var deleteButton = document.getElementsByClassName("del_button")
-  //   for (var i = 0; i < deleteButton.length; i++) {
-  //     deleteButton[i].addEventListener("click", deleteMessage);
-  //   }
-  // }
-  // function deleteMessage(event) {
-  //   var listParent = event.target.parentElement;
-    
-  //   originalChatty.removeChild(listParent);   
-  // }
+ // ------- Event listener and conditionals for delete messages --------------- //
+
+function addeventsonDeleteButton() {
+let deleteButtonListener = document.getElementsByClassName("dButton")
+
+console.log("deletebutton", deleteButtonListener);
+console.log("length", deleteButtonListener.length);
+
+for (let i = 0; i < deleteButtonListener.length; i++) {
+   let deleteButton = deleteButtonListener[i]
+   console.log("deltebuttontest", deleteButton );
+   deleteButton.addEventListener("click", youClickedMe);
+   console.log("deletebutton", deleteButtonListener[i]);
+   };
+ };
+
+// ------- Event Delete the message selected --------------- //
+
+ function youClickedMe(event){
+    let listParent = event.target.parentElement;
+    console.log("youClickedMe", event.target.parentElement );
+    console.log("listParent", listParent );
+    textArea.removeChild(listParent);  
+ };
+
+
+// ------- Event listener and conditionals clear messages addded --------------- //
+
+function addEventOnMessagesClearButton(){
+  console.log("inclearbutton");
+  let clearButtonListener = document.getElementsByClassName("btn btn-default")
+  clearButton = clearButtonListener.addEventListener("click", function(){
+    Chatty.addMessagesToArray();
+    clearButton.disabled=true;
+  })
+};
