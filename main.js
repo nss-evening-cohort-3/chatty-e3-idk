3b545f9b30d81008350e9db590ca730a821d9af3
complikatyed <katye.russell@gmail.com>
2016-03-24 23:13:42 -0500

    Refactor to move message functions into iife

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict'
-// This is the main iife that gets augmented by our individual js files
+
+// This is the main iife that gets augmented by the other js files
 
 var Chatty = (function() {
 
@@ -12,48 +13,6 @@ var Chatty = (function() {
   };
 
 })();
-
-// ------ Sends the message string (of JSON messages) to the DOM --------------- //
-
-  let showMessages = function(ourText, ourId) {
-
-    let messageArea = document.getElementById(ourId);
-
-    messageArea.innerHTML = ourText;
-
-  }
-
-// ---------- Push JSON messages to myMessages array --------------------------//
-
-  let gatherMessages = function(messages) {
-
-    let messagesArray;
-
-    messages.forEach(function (message, index) {
-
-      messagesArray = Chatty.getArray();
-      messagesArray.push(message);
-
-    });
-
-    buildMessages(messagesArray);
-  }
-
-// ------ Builds a message string from the JSON message data (loaded by load.js) --------------- //
-
-  let buildMessages = function(items) {
-
-    let ourMessages = "";
-
-    items.forEach(function(item, index) {
-
-     ourMessages += `<section id="message--${index}"> ${item.text}`;
-     ourMessages += `<button id="btn--${index} class="del_button">Delete</button>`;
-     ourMessages += `</section>`;
-    });
-
-    showMessages(ourMessages, "textArea");
-  }
 
 
 // ------- Event listener and conditionals for dark-theme checkbox --------------- //
