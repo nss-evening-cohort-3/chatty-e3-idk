a396a8e7ebe37d14b537979b2341bc21d5e8f124
complikatyed <katye.russell@gmail.com>
2016-03-24 21:57:40 -0500

    Still working on possible add iffe

--- add.js
+++ add.js
@@ -7,13 +7,12 @@
 // One IIFE should accept an element `id`, and the user message, and then add the user's message - along with the delete button - to the specified parent element. Each message should be stored in a private array in this IIFE. This IIFE should expose a function to read all messages, and delete a single message.
 
 "use strict";
-var Chatty = (function(originalChatty){
+var Chatty = (function(originalChatty) {
 
   // Message comes in and gets stored in the array
 
   // Messages from the array get added to the DOM
 
-  let allMessages = [];
 
   originalChatty.getMessage = function(elId, message){
     
